11da5cb1d3ecd144355db6e2da480e34126fb635
Paulo Mulotto <paulo.mulotto@gmail.com>
2019-08-24 16:20:28 -0300

    correcao sh

--- lab2/c_lang/exec.sh
+++ lab2/c_lang/exec.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -x
 
-sed -E -e "/TEST|test/d" code1.c > tmp_code_sed.c
-sed -e "s/INT\>\|CHAR\>\|FLOAT\>\|LONG\>\|VOID\>\|ENUM\>\|STRUCT\>\|UNION\>\|TYPEDEF\>\|FOR\>\|WHILE\>\|IF\>\|ELSE\>\|SWITCH\>\|CASE\>\|BREAK\>\|CONTINUE\>\|RETURN\>\|GOTO\>/\L&/g"  tmp_code_sed.c > code1_sed.c
-
-sed -E -e "/TEST|test/d" code2.c > tmp_code_sed.c
-sed -e "s/INT\>\|CHAR\>\|FLOAT\>\|LONG\>\|VOID\>\|ENUM\>\|STRUCT\>\|UNION\>\|TYPEDEF\>\|FOR\>\|WHILE\>\|IF\>\|ELSE\>\|SWITCH\>\|CASE\>\|BREAK\>\|CONTINUE\>\|RETURN\>\|GOTO\>/\L&/g"  tmp_code_sed.c > code2_sed.c
-
-sed -E -e "/TEST|test/d" code3.c > tmp_code_sed.c
-sed -e "s/INT\>\|CHAR\>\|FLOAT\>\|LONG\>\|VOID\>\|ENUM\>\|STRUCT\>\|UNION\>\|TYPEDEF\>\|FOR\>\|WHILE\>\|IF\>\|ELSE\>\|SWITCH\>\|CASE\>\|BREAK\>\|CONTINUE\>\|RETURN\>\|GOTO\>/\L&/g"  tmp_code_sed.c > code3_sed.c
-
-sed -E -e "/TEST|test/d" code4.c > tmp_code_sed.c
-sed -e "s/INT\>\|CHAR\>\|FLOAT\>\|LONG\>\|VOID\>\|ENUM\>\|STRUCT\>\|UNION\>\|TYPEDEF\>\|FOR\>\|WHILE\>\|IF\>\|ELSE\>\|SWITCH\>\|CASE\>\|BREAK\>\|CONTINUE\>\|RETURN\>\|GOTO\>/\L&/g"  tmp_code_sed.c > code4_sed.c
+sed -E -e "/TEST|test/d" $1 > tmp_code_sed.c
+sed -e "s/INT\>\|CHAR\>\|FLOAT\>\|LONG\>\|VOID\>\|ENUM\>\|STRUCT\>\|UNION\>\|TYPEDEF\>\|FOR\>\|WHILE\>\|IF\>\|ELSE\>\|SWITCH\>\|CASE\>\|BREAK\>\|CONTINUE\>\|RETURN\>\|GOTO\>/\L&/g"  tmp_code_sed.c > corrected.c
 
 rm tmp_code_sed.c
 
 
 flex -i -o scanner.c scanner.l
 gcc -g -o scanner_test scanner_test.c scanner.c
-./scanner_test < $1
+./scanner_test < corrected.c
